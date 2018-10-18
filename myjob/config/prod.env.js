6fb096550ce5ab207f51f162af47f491de07b99f
lds <sanda0607@163.com>
2018-10-18 23:00:22 +0800

    测试自己打包

--- myjob/config/prod.env.js
+++ myjob/config/prod.env.js
@@ -4,5 +4,5 @@ module.exports = {
   SET_DOT: '"https://pv.mama100.com/statistics/api/add"',
   SWISSE: '"http://www.swisse-china.com.cn"', //授权地址，打包到生成的时候要改为 http://www.swisse-china.com.cn
   LOGIN: '"http://www.swisse-china.com.cn/swisse-wmall/center/myAccount/add_customer.html"', //登录注册地址，打包到生产的时候要改为http://www.swisse-china.com.cn/swisse-wmall/center/myAccount/add_customer.html
-  ROATER:'"/qxby-h5"'
+  ROATER:'"/"'
 }
